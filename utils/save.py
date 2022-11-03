6b548b11a44c3e30f06a55afc69e79b96c0a2c91
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2022-11-02 20:11:58 -0700

    made changes to incorporate saving multiple files with different names.

--- utils/save.py
+++ utils/save.py
@@ -3,9 +3,7 @@
 from stl import mesh, stl
 from numpy import concatenate
 
-def meshlist2stl(mesh_list):
+def meshlist2stl(mesh_list,dim,shape):
     
     justonemesh = mesh.Mesh( concatenate( [me.data for me in mesh_list]))
-    justonemesh.save('mesh_X_try.stl', mode= stl.Mode.ASCII)
-    
-#TODO: other save modes??
+    justonemesh.save(f'./required_meshes/{shape}_{dim}.stl', mode= stl.Mode.ASCII)
